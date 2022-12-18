acb72de8b8c87fa2675901d4bef35069be851b59
Woncheol Kim <icijiy@gmail.com>
2022-12-17 22:56:37 -0800

    cartItem document querySelector

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
   navbar.classList.toggle('active');
 }
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+  cartItem.classList.toggle('active');
+}
